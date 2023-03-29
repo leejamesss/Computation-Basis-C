283d1c4e5abdc8cf5d01c4e3236da5c22b1eb01a
james <117844938+leejamesss@users.noreply.github.com>
2023-03-29 11:55:59 +0800

    Update 2.py

--- hw05/2.py
+++ hw05/2.py
@@ -1,1 +1,18 @@
 
+str=input()
+dict={}
+for ch in str:
+   if ch in dict:
+         dict[ch]+=1
+   else:
+         dict[ch]=1
+#输出值最大的键和值
+max=0
+for key,value in dict.items():
+   if value>max:
+         max=value
+for key,value in dict.items():
+   if value==max:
+         print(key,value)
+
+
