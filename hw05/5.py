7a61e9d98d0e6cfa80420a11db1c2ac22b591c3c
james <117844938+leejamesss@users.noreply.github.com>
2023-03-29 12:52:22 +0800

    Update 5.py

--- hw05/5.py
+++ hw05/5.py
@@ -1,1 +1,27 @@
+begin=input().split(":")
+end=input().split(":")
+preRecord=7*60
+afterRecord=23*60
+Mid=12*60
+beginTime=int(begin[0])*60+int(begin[1])
+endTime=int(end[0])*60+int(end[1])
+reduce=0
+def time(beginTime,endTime,reduce):
+
+      if beginTime<Mid and endTime>Mid:
+         reduce=reduce+30
+      if beginTime<preRecord:
+         beginTime=preRecord
+      if endTime>afterRecord:
+         endTime=afterRecord
+      total=endTime-beginTime-reduce
+      hour=(endTime-beginTime-reduce)//60
+      min=(endTime-beginTime-reduce)%60
+      if total<0:
+         print("0小时")
+      else:
+         print("%d小时%d分钟"%(hour,min))
+time(beginTime,endTime,reduce)
+
+
 
