410e3e1b5a2987a26fee478545676ed068626294
james <117844938+leejamesss@users.noreply.github.com>
2023-03-29 11:49:31 +0800

    Update 1.py

--- hw05/1.py
+++ hw05/1.py
@@ -1,1 +1,14 @@
 
+str=input()
+num=0
+strcnt=0
+other=0
+for ch in str:
+    if ch.isdigit():
+       num+=1
+    elif ch.isalpha():
+       strcnt+=1
+    else:
+       other+=1
+dict={'字母':strcnt,"数字":num,'其他':other}
+print(dict) 
