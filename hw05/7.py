5af18bf691eae1b8cda2a20d88683172f2cbf563
james <117844938+leejamesss@users.noreply.github.com>
2023-03-29 13:07:16 +0800

    Update 7.py

--- hw05/7.py
+++ hw05/7.py
@@ -1,1 +1,20 @@
 
+n=int(input())
+lst=input().split(",")
+birthlst=[]
+for i in range(n):
+    id=input()
+    birthlst.append(int(id[10:12]))
+for j in lst:
+    cnt=birthlst.count(int(j))
+    if cnt==0:
+        print("活动取消")
+    elif 1<=cnt and cnt<=5:
+        print("静园草坪")
+    elif 6<=cnt and cnt<=10:
+        print("百讲")
+    else:
+        print("邱德拔")
+
+        
+
