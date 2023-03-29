fcf53c14a7bcccfcfc17f214573716ef41e768ca
james <117844938+leejamesss@users.noreply.github.com>
2023-03-29 13:00:45 +0800

    Update 6.py

--- hw05/6.py
+++ hw05/6.py
@@ -1,1 +1,20 @@
 
+lst=input().split(",")
+def isvalid(password):
+      if len(password)<6 or len(password)>12:
+         return False
+      if not any(char.isdigit() for char in password):
+         return False
+      if not any(char.isupper() for char in password):
+         return False
+      if not any(char.islower() for char in password):
+         return False
+      if not any(char in ["@","#","$","!"] for char in password):
+         return False
+      if any((not char.isdigit()) and (not char.isupper()) and (not char.islower()) and (not char in ["@","#","$","!"]) for char in password):
+         return False
+      return True
+for password in lst:
+    if isvalid(password):
+       print(password)
+    
